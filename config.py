31850b9ed1d8d25cb5ce6d682f3e888a3b5008cb
feelinglucky142007-spec <feelinglucky142007@gmail.com>
2026-08-04 01:08:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,10 +45,10 @@ MESSAGES = {
     "ABOUT": "<b><blockquote>◈ ᴏᴡɴᴇʀ : <a href=t.me/Im_Sukuna02>ɪᴍ•Ꮪᴜ͢ᴋᴜɴᴀ</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/InfinixSyndicate>ɪɴғɪɴɪx sʏɴᴅɪᴄᴀᴛᴇ</a>\n◈ ᴍᴏᴠɪᴇs ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Infinix_Movie>ɪɴғɪɴɪx ᴍᴏᴠɪᴇs</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/Im_Sukuna02>Sᴜᴋᴜɴᴀ</a></blockquote></b>",
     "REPLY": "<b>For More Join - @Infinix_Adult</b>",
     "SHORT_MSG": "<b>📊 ʜᴇʏ {first}, \n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n ⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ ᴏᴘᴇɴ ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>",
-    "START_PHOTO": "https://i.ibb.co/GvJsGpyL/x.jpg",
-    "FSUB_PHOTO": "https://i.ibb.co/GvJsGpyL/x.jpg",
-    "SHORT_PIC": "https://i.ibb.co/GvJsGpyL/x.jpg",
-    "SHORT": "https://i.ibb.co/GvJsGpyL/x.jpg"
+    "START_PHOTO": "https://i.ibb.co/FkYfp1wq/x.jpg",
+    "FSUB_PHOTO": "https://i.ibb.co/FkYfp1wq/x.jpg",
+    "SHORT_PIC": "https://i.ibb.co/FkYfp1wq/x.jpg",
+    "SHORT": "https://i.ibb.co/FkYfp1wq/x.jpg"
 }
 
 def LOGGER(name: str, client_name: str) -> logging.Logger:
